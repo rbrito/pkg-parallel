6725ac82fdbbe13f366e667061b6d07d0548d05c
Rogério Brito <rbrito@ime.usp.br>
2020-01-16 00:56:33 -0300

    New upstream version 20191222

--- src/env_parallel.fish
+++ src/env_parallel.fish
@@ -2,13 +2,12 @@
 
 # This file must be sourced in fish:
 #
-#   source (which env_parallel.fish)
+#   . (which env_parallel.fish)
 #
 # after which 'env_parallel' works
 #
 #
-# Copyright (C) 2016
-# Ole Tange and Free Software Foundation, Inc.
+# Copyright (C) 2016-2019 Ole Tange and Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -42,6 +41,17 @@
 
 function env_parallel
   # env_parallel.fish
+
+  # --session
+  perl -e 'exit grep { /^--session/ } @ARGV' -- $argv; or begin;
+    setenv PARALLEL_IGNORED_NAMES (
+        begin;
+	  functions -n
+	  set -n;
+	end | perl -pe 's/\n/,/g';
+    )
+    return 0
+  end;
   setenv PARALLEL_ENV (
     begin;
       set _grep_REGEXP (
@@ -71,10 +81,14 @@ function env_parallel
             	    "Run \"parallel --record-env\" in a clean environment first.\n";
                 } else {
             	    chomp(@ignored_vars = <IN>);
-            	    $vars = join "|",map { quotemeta $_ } "env_parallel", @ignored_vars;
-		    print $vars ? "($vars)" : "(,,nO,,VaRs,,)";
                 }
             }
+            if($ENV{PARALLEL_IGNORED_NAMES}) {
+                push @ignored_vars, split/,/, $ENV{PARALLEL_IGNORED_NAMES};
+                chomp @ignored_vars;
+            }
+            $vars = join "|",map { quotemeta $_ } @ignored_vars;
+	    print $vars ? "($vars)" : "(,,nO,,VaRs,,)";
             ' -- $argv;
         end;
       )
@@ -88,18 +102,46 @@ function env_parallel
       end;
 
       # Export function definitions
-      functions -n | perl -pe 's/,/\n/g' | grep -E "^$_grep_REGEXP"\$ | grep -vE "^$_ignore_UNDERSCORE"\$ | while read d; functions $d; end;
+      # Keep the ones from --env
+      # Ignore the ones from ~/.parallel/ignored_vars
+      # Dump each function defition
+      # Replace \001 with \002 because \001 is used by env_parallel
+      # Convert \n to \001
+      functions -n | perl -pe 's/,/\n/g' | \
+        grep -Ev '^(PARALLEL_ENV|PARALLEL_TMP)$' | \
+        grep -E "^$_grep_REGEXP"\$ | grep -vE "^$_ignore_UNDERSCORE"\$ | \
+        while read d; functions $d; end | \
+        perl -pe 's/\001/\002/g and not $printed++ and print STDERR
+          "env_parallel: Warning: ASCII value 1 in functions is not supported\n";
+                  s/\n/\001/g';
       # Convert scalar vars to fish \XX quoting
-      eval (set -L | grep -E "^$_grep_REGEXP " | grep -vE "^$_ignore_UNDERSCORE " | perl -ne 'chomp;
-        ($name,$val)=split(/ /,$_,2);
-        $name=~/^(HOME|USER|COLUMNS|FISH_VERSION|LINES|PWD|SHLVL|_|history|status|version)$|\./ and next;
-        if($val=~/^'"'"'/) { next; }
-        print "set $name \"\$$name\";\n";
-      ')
+      # Keep the ones from --env
+      # Ignore the ones from ~/.parallel/ignored_vars
+      # Ignore read only vars
+      # Execute 'set' of the content
+      eval (set -L | \
+        grep -Ev '^(PARALLEL_TMP)$' | \
+        grep -E "^$_grep_REGEXP " | grep -vE "^$_ignore_UNDERSCORE " | \
+        perl -ne 'chomp;
+          ($name,$val)=split(/ /,$_,2);
+          $name=~/^(HOME|USER|COLUMNS|FISH_VERSION|LINES|PWD|SHLVL|_|
+                    history|status|version)$|\./x and next;
+          if($val=~/^'"'"'/) { next; }
+          print "set $name \"\$$name\";\n";
+        ')
       # Generate commands to set scalar variables
+      # Keep the ones from --env
+      # Ignore the ones from ~/.parallel/ignored_vars
+      #
       begin;
-        for v in (set -n | grep -E "^$_grep_REGEXP\$" | grep -vE "^$_ignore_UNDERSCORE\$");
+        for v in (set -n | \
+          grep -Ev '^(PARALLEL_TMP)$' | \
+          grep -E "^$_grep_REGEXP\$" | grep -vE "^$_ignore_UNDERSCORE\$");
           # Separate variables with the string: \000
+	  # array_name1 val1\0
+	  # array_name1 val2\0
+	  # array_name2 val3\0
+	  # array_name2 val4\0
           eval "for i in \$$v;
             echo -n $v \$i;
     	    perl -e print\\\"\\\\0\\\";
@@ -108,33 +150,39 @@ function env_parallel
         # A final line to flush the last variable in Perl
         perl -e print\"\\0\";
       end | perl -0 -ne '
-          # Remove separator string
-          chop;
-          ($name,$val)=split(/ /,$_,2);
-          # Ignore read-only vars
-          $name=~/^(HOME|USER|COLUMNS|FISH_VERSION|LINES|PWD|SHLVL|_|history|status|version)$/ and next;
-          # Quote $val
-          $val=~s/[\002-\011\013-\032\\\#\?\`\(\)\{\}\[\]\^\*\<\=\>\~\|\; \"\!\$\&\202-\377]/\\\$&/go;
-          # Quote single quote
-          $val=~s/'"'"'/\\\$&/go;
-          # Quote newline as '\n'
-          $val =~ s/[\n]/\\\n/go;
- 	  # Empty => 2 single quotes = \047\047
-	  $val=~s/^$/\047\047/o;
-          if($name ne $last and $last) {
-            # The $name is different, so this is a new variable.
-            # Print the last one.
-            # Separate list elements by 2 spaces
-            $"="  ";
-            print "set $last @qval;\n";
-            @qval=();
-          }
-          push @qval,$val;
-          $last=$name;
-        ';
-    end |perl -pe 's/\001/\\cb/g and print STDERR "env_parallel: Warning: ASCII value 1 in variables is not supported\n";
-                   s/\n/\001/'
+        # Remove separator string \0
+        chop;
+	# Split line into name and value
+        ($name,$val)=split(/ /,$_,2);
+        # Ignore read-only vars
+        $name=~/^(HOME|USER|COLUMNS|FISH_VERSION|LINES|PWD|SHLVL|_|
+                  history|status|version)$/x and next;
+        # Single quote $val
+	if($val =~ /[^-_.+a-z0-9\/]/i) {
+	  $val =~ s/\047/\047"\047"\047/g;  # "-quote single quotes
+  	  $val = "\047$val\047";            # single-quote entire string
+	  $val =~ s/^\047\047|\047\047$//g; # Remove unneeded '' at ends
+	} elsif ($val eq "") {
+	  $val = "\047\047";
+	}
+
+        if($name ne $last and $last) {
+          # The $name is different, so this is a new variable.
+          # Print the last one.
+          # Separate list elements by 2 spaces
+          $"="  ";
+          print "set $last @qval;\n";
+          @qval=();
+        }
+        push @qval,$val;
+        $last=$name;
+        '| \
+        perl -pe 's/\001/\002/g and not $printed++ and print STDERR
+          "env_parallel: Warning: ASCII value 1 in variables is not supported\n";
+          s/\n/\001/g'
+    end;
     )
+  # If --record-env: exit
   perl -e 'exit grep { /^--record-env$/ } @ARGV' -- $argv; and parallel $argv;
   set _parallel_exit_CODE $status
   set -e PARALLEL_ENV
--- src/env_parallel.tcsh
+++ src/env_parallel.tcsh
@@ -7,8 +7,7 @@
 # after which 'env_parallel' works
 #
 #
-# Copyright (C) 2016
-# Ole Tange and Free Software Foundation, Inc.
+# Copyright (C) 2016-2019 Ole Tange and Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,15 +25,19 @@
 # Fifth Floor, Boston, MA 02110-1301 USA
 
 set _parallel_exit_CODE=0
-if ("`alias env_parallel`" == '') then
+if ("`alias env_parallel`" == '' || ! $?PARALLEL_CSH) then
   # Activate alias
-  alias env_parallel 'setenv PARALLEL "\!*"; source `which env_parallel.tcsh`'
+  alias env_parallel '(setenv PARALLEL_CSH "\!*"; source `which env_parallel.csh`)'
 else
   # Get the --env variables if set
   # --env _ should be ignored
   # and convert  a b c  to (a|b|c)
   # If --env not set: Match everything (.*)
-  set _tMpscRIpt=`tempfile`
+
+  # simple 'tempfile': Return nonexisting filename: /tmp/parXXXXX
+  alias _tempfile 'perl -e do\{\$t\=\"/tmp/par\".join\"\",map\{\(0..9,\"a\"..\"z\",\"A\"..\"Z\"\)\[rand\(62\)\]\}\(1..5\)\;\}while\(-e\$t\)\;print\"\$t\\n\"'
+  set _tMpscRIpt=`_tempfile`
+
   cat <<'EOF' > $_tMpscRIpt
             #!/usr/bin/perl
 
@@ -46,12 +49,12 @@ else
             $vars = join "|",map { quotemeta $_ } @envvar;
             print $vars ? "($vars)" : "(.*)";
 'EOF'
-  set _grep_REGEXP="`perl $_tMpscRIpt -- $PARALLEL`"
+  set _grep_REGEXP="`perl $_tMpscRIpt -- $PARALLEL_CSH`"
 
   # Deal with --env _
   cat <<'EOF' > $_tMpscRIpt
             #!/usr/bin/perl
-            
+
             for(@ARGV){
                 $next_is_env and push @envvar, split/,/, $_;
                 $next_is_env=/^--env$/;
@@ -62,23 +65,24 @@ else
             	    "Run \"parallel --record-env\" in a clean environment first.\n";
                 } else {
             	    chomp(@ignored_vars = <IN>);
-            	    $vars = join "|",map { quotemeta $_ } "env_parallel", @ignored_vars;
+            	    $vars = join "|",map { quotemeta $_ } @ignored_vars;
 		    print $vars ? "($vars)" : "(,,nO,,VaRs,,)";
                 }
             }
 'EOF'
-  set _ignore_UNDERSCORE="`perl $_tMpscRIpt -- $PARALLEL`"
+  set _ignore_UNDERSCORE="`perl $_tMpscRIpt -- $PARALLEL_CSH`"
   rm $_tMpscRIpt
 
   # Get the scalar and array variable names
-  set _vARnAmES=(`set | awk -e '{print $1}' |grep -vE '^(#|_|killring|prompt2|command)$' | grep -E "^$_grep_REGEXP"\$ | grep -vE "^$_ignore_UNDERSCORE"\$`)
+  set _vARnAmES=(`set | perl -ne 's/\s.*//; /^(#|_|killring|prompt2|command|PARALLEL_ENV|PARALLEL_TMP)$/ and next; /^'"$_grep_REGEXP"'$/ or next; /^'"$_ignore_UNDERSCORE"'$/ and next; print'`)
 
   # Make a tmpfile for the variable definitions
-  set _tMpvARfILe=`tempfile`
-  
+  set _tMpvARfILe=`_tempfile`
+  touch $_tMpvARfILe
   # Make a tmpfile for the variable definitions + alias
-  set _tMpaLLfILe=`tempfile`
+  set _tMpaLLfILe=`_tempfile`
   foreach _vARnAmE ($_vARnAmES);
+    # These 3 lines break in csh version 20110502-3
     # if not defined: next
     eval if'(! $?'$_vARnAmE') continue'
     # if $#myvar <= 1 echo scalar_myvar=$var
@@ -87,7 +91,7 @@ else
     eval if'(${#'$_vARnAmE'} > 1) echo array_'$_vARnAmE'="$'$_vARnAmE'"' >> $_tMpvARfILe;
   end
   unset _vARnAmE _vARnAmES
-  # shell quote variables (--plain needed due to $PARALLEL abuse)
+  # shell quote variables (--plain needed due to ignore if $PARALLEL is set)
   # Convert 'scalar_myvar=...' to 'set myvar=...'
   # Convert 'array_myvar=...' to 'set array=(...)'
   cat $_tMpvARfILe | parallel --plain --shellquote |  perl -pe 's/^scalar_(\S+).=/set $1=/ or s/^array_(\S+).=(.*)/set $1=($2)/ && s/\\ / /g;' > $_tMpaLLfILe
@@ -120,20 +124,15 @@ else
 #   s/^/\001alias /;
 #   Quoted: s/\^/\\001alias\ /\;
   alias | \
-    grep -E "^$_grep_REGEXP" | \
-    grep -vE "^$_ignore_UNDERSCORE""[^_a-zA-Z]" | \
-    perl -pe s/\\047/\\047\\042\\047\\042\\047/g\;s/\^\(\\S+\)\(\\s+\)\\\(\(.\*\)\\\)/\\1\\2\\3/\;s/\^\(\\S+\)\(\\s+\)\(.\*\)/\\1\\2\\047\\3\\047/\;s/\^/\\001alias\ /\;s/\\\!/\\\\\\\!/g >> $_tMpaLLfILe 
-  
+    perl -ne '/^'"$_grep_REGEXP"'/ or next; /^'"$_ignore_UNDERSCORE"'[^_a-zA-Z]/ and next; print' | \
+    perl -pe s/\\047/\\047\\042\\047\\042\\047/g\;s/\^\(\\S+\)\(\\s+\)\\\(\(.\*\)\\\)/\\1\\2\\3/\;s/\^\(\\S+\)\(\\s+\)\(.\*\)/\\1\\2\\047\\3\\047/\;s/\^/\\001alias\ /\;s/\\\!/\\\\\\\!/g >> $_tMpaLLfILe
+
   setenv PARALLEL_ENV "`cat $_tMpaLLfILe; rm $_tMpaLLfILe`";
   unset _tMpaLLfILe;
-  # Use $PARALLEL set in calling alias
+  # Use $PARALLEL_CSH set in calling alias
   parallel
   set _parallel_exit_CODE=$status
   setenv PARALLEL_ENV
-  setenv PARALLEL
+  setenv PARALLEL_CSH
 endif
 (exit $_parallel_exit_CODE)
-
-# Tested working for aliases
-# alias env_parallel 'setenv PARALLEL_ENV "`alias | perl -pe s/\\047/\\047\\042\\047\\042\\047/g\;s/\^\(\\S+\)\(\\s+\)\\\(\(.\*\)\\\)/\\1\\2\\3/\;s/\^\(\\S+\)\(\\s+\)\(.\*\)/\\1\\2\\047\\3\\047/\;s/\^/\\001alias\ /\;s/\\\!/\\\\\\\!/g;`";parallel \!*; setenv PARALLEL_ENV'
-
